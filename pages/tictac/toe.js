e668b3876fe12033d32e2a24c687328d28da0a5a
Nathaniel Nasarow <nasarow@epigno.jp>
2023-03-17 17:44:58 +0900

    [pages] tictac: Modifies visuals and adds a board for reference
    Signed-off-by: Nathaniel Nasarow <nasarow@epigno.jp>

--- pages/tictac/toe.js
+++ pages/tictac/toe.js
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+import styles from '../../styles/Toe.module.css';
+
 export default function ticTacText() {
   return (
-    <h1>Welcome To TicTacToe</h1>
+    <>
+      <h1>Welcome To TicTacToe</h1>
+
+      <Card></Card>
+    </>
+  )
+}
+
+function Card() {
+  return (
+    <div className={`${styles.card} ${styles['text-center']}`}>
+      <div className={styles.row}>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className={styles.row}>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className={styles.row}>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+    </div>
+  )
+}
+function Square() {
+  return (
+    <button className={styles['square-btn']}>X</button>
   )
 }
