281bd3b793c822783d60492b522e1a73e6986501
Nathaniel Nasarow <nasarow@epigno.jp>
2023-03-18 17:12:56 +0900

    [toe] Adds logic for checking and returning the winner of a game
    Signed-off-by: Nathaniel Nasarow <nasarow@epigno.jp>

--- pages/tictac/toe.js
+++ pages/tictac/toe.js
@@ -2,25 +2,74 @@ import { useState } from 'react';
 import styles from '../../styles/Toe.module.css';
 
 export default function ticTacText() {
+  const [ turnIsX, setTurn ] = useState(true)
+  const [ hasWinner, setHasWinner ] = useState(false)
+  const currentTurn = turnIsX ? 'X' : 'O'
+
+  const handleTurn = (squares) => {
+    setHasWinner(checkForWinner(squares))
+    if (!checkForWinner(squares)) {
+      setTurn(!turnIsX)
+    }
+  }
+
+
   return (
     <>
       <h1>Welcome To TicTacToe</h1>
-
-      <Board></Board>
+      <TurnOrWinner hasWinner={hasWinner} currentTurn={currentTurn} />
+      <Board currentTurn={currentTurn} changeTurn={(squares) => handleTurn(squares)} hasWinner={hasWinner}></Board>
     </>
   )
 }
 
-function Board() {
+function checkForWinner(squares) {
+  const winningSlots = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
+
+  for (let i = 0; i < winningSlots.length; i++) {
+    const [a, b, c] = winningSlots[i]
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return true
+    }
+  }
+  return false
+}
+
+
+function TurnOrWinner(props) {
+  if (props.hasWinner) {
+    return (
+      <>
+        <h1>We have a winner!</h1>
+        <h4>Congratulations, Player {props.currentTurn}</h4>
+      </>
+    )
+  } else {
+    return (
+      <h1>Current Player: {props.currentTurn}</h1>
+    )
+  }
+}
+
+function Board({currentTurn, changeTurn, hasWinner}) {
   const [ squares, setSquares ] = useState(Array(9).fill(null))
-  const [ turnIsX, setTurn ] = useState(true)
 
   function handleClick(i) {
-    if (squares[i]) return
+    if (squares[i] || hasWinner) return
     const nextSquares = squares.slice()
-    nextSquares[i] = turnIsX ? 'X' : 'O'
+    nextSquares[i] = currentTurn
     setSquares(nextSquares)
-    setTurn(!turnIsX)
+    changeTurn(nextSquares)
   }
 
   return (
