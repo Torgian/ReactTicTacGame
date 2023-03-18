58c0cf94d211a731b7d58b145e4620981ea76f91
Nathaniel Nasarow <nasarow@epigno.jp>
2023-03-18 13:46:44 +0900

    [toe] Add some basic functionality
    Signed-off-by: Nathaniel Nasarow <nasarow@epigno.jp>

--- pages/tictac/toe.js
+++ pages/tictac/toe.js
@@ -12,44 +12,47 @@ export default function ticTacText() {
 }
 
 function Board() {
+  const [ squares, setSquares ] = useState(Array(9).fill(null))
+
+  let currentTurn = 'X';
+
+  function handleClick(i) {
+    if (squares[i]) return
+    const nextSquares = squares.slice()
+    nextSquares[i] = currentTurn
+    setSquares(nextSquares)
+  }
+
   return (
     <div className={`${styles.card} ${styles['text-center']}`}>
       <div className={styles.row}>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
       </div>
       <div className={styles.row}>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
       </div>
       <div className={styles.row}>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
       </div>
     </div>
   )
 }
 
-let currentPlayer = 'X';
 
-function Square() {
-  const [ turn, setTurn ] = useState(null);
 
-  const handleClick = () => {
-    if (turn) return
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    setTurn(currentPlayer);
-  }
-
+function Square({value, onSquareClick}) {
   return (
     <button
       className={styles['square-btn']}
-      onClick={handleClick}
+      onClick={onSquareClick}
       >
-        {turn}
+        {value}
     </button>
   )
 }
